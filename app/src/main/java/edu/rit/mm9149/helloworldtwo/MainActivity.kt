18e6c73cce69557d92c0ea4c264a4a41dfa6e2f7
markm <mm9149@g.rit.edu>
2023-01-18 18:52:07 +0100

    New project

--- app/src/main/java/edu/rit/mm9149/helloworldtwo/MainActivity.kt
+++ app/src/main/java/edu/rit/mm9149/helloworldtwo/MainActivity.kt
@@ -3,26 +3,47 @@ package edu.rit.mm9149.helloworldtwo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.rit.mm9149.helloworldtwo.ui.theme.HelloWorldTwoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorldTwoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+                CounterMessage()
         }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = (msg.author))
+        Text(text = (msg.body))
+    }
+}
+
+@Composable
+fun CounterMessage() {
+    var counter = 0
+    val context = LocalContext.current
+
+    Column {
+        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.padding(all = 30.dp)
     }
 }
 
@@ -35,6 +56,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     HelloWorldTwoTheme {
-        Greeting("Android")
+        MessageCard(msg = Message("Mark", "Hello Mark"))
     }
 }
