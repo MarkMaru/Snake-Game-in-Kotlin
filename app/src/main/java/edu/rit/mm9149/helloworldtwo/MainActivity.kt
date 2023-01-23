5a529da8bdb583625e0ed8f088bcadf0aad5ec29
markm <mm9149@g.rit.edu>
2023-01-23 18:45:33 +0100

    New project

--- app/src/main/java/edu/rit/mm9149/helloworldtwo/MainActivity.kt
+++ app/src/main/java/edu/rit/mm9149/helloworldtwo/MainActivity.kt
@@ -1,20 +1,25 @@
 package edu.rit.mm9149.helloworldtwo
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.rit.mm9149.helloworldtwo.ui.theme.HelloWorldTwoTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,24 +32,86 @@ class MainActivity : ComponentActivity() {
 }
 
 data class Message(val author: String, val body: String)
-
 @Composable
 fun MessageCard(msg: Message) {
     Column {
-        Text(text = (msg.author))
-        Text(text = (msg.body))
+        Text(text = "Hello ${msg.author}")
+        Text(text = msg.body)
     }
 }
 
 @Composable
 fun CounterMessage() {
-    var counter = 0
+//    var countState by remember {
+//        mutableStateOf(0)
+//    }
+
+    var countState by rememberSaveable {
+        mutableStateOf(0)
+    }
+
     val context = LocalContext.current
 
-    Column {
-        horizontalAlignment = Alignment.CenterHorizontally
-        modifier = Modifier.padding(all = 30.dp)
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(all = 30.dp)
+            .fillMaxWidth()
+    ) {
+        Surface(shape = MaterialTheme.shapes.medium, elevation = 10.dp) {
+            Text(text = "$countState",
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.h1,
+                modifier = Modifier
+                    .then(if (countState > 5) Modifier.background(Color.Red) else Modifier)
+                    .then(if (countState > 15) Modifier.background(Color.Green) else Modifier)
+                    .padding(all = 20.dp),
+                fontSize = 72.sp)
+        }
+        
+        Spacer(modifier = Modifier.padding(all = 8.dp))
+
+        ButtonRow(count = countState,
+            updateCount = {
+            newCount -> countState = newCount
+        })
     }
+}
+
+@Composable
+fun CountButton(count: Int, updateCount: (Int) -> Unit) {
+
+    Button(onClick = {
+        updateCount(count + 1)
+    }) {
+        Text(text = "Count")
+    }
+
+}
+
+@Composable
+fun ToastButton(count: Int) {
+
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Toast.makeText(context, " The count is $count", Toast.LENGTH_LONG).show()
+    }) {
+        Text("Toast")
+    }
+
+}
+
+@Composable
+fun ButtonRow(count: Int, updateCount: (Int) -> Unit) {
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+        ToastButton(count = count)
+        Spacer(modifier = Modifier.padding(all = 8.dp))
+        CountButton(count = count, updateCount = updateCount)
+    }
+
 }
 
 @Composable
@@ -52,10 +119,22 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    HelloWorldTwoTheme {
+//        CounterMessage()
+//    }
+//}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun DefaultPreview() {
+fun PreviewCounterMessage() {
     HelloWorldTwoTheme {
-        MessageCard(msg = Message("Mark", "Hello Mark"))
+        CounterMessage()
     }
 }
